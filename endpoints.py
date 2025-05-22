5b19b1c103f16593518bf91ade40c04478b02a69
Justin Watanasiri <justin.watanasiri@gmail.com>
2025-05-22 17:19:08 -0400

    removing unused endpoint

--- endpoints.py
+++ endpoints.py
@@ -2,4 +2,3 @@ baseURL = 'https://places.googleapis.com/'
 
 urlSearchNearby = baseURL + 'v1/places:searchNearby'
 urlSearchText = baseURL + 'v1/places:searchText'
-urlSWAPI = 'https://www.swapi.tech/api/people/1'
