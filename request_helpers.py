1881c8cb84aa4db0b9801ed57c6f7a5b5c378b57
Justin Watanasiri <justin.watanasiri@gmail.com>
2025-05-22 17:47:52 -0400

    request_helpers comments

--- request_helpers.py
+++ request_helpers.py
@@ -6,7 +6,8 @@ from auth import get_api_key
 apiKey = get_api_key('api_key.txt')
 
 
-def searchNearby():
+# This function isn't used in the current version of the code but is left here for possible future use.
+def searchNearby(lat, long, radius):
     headers = {
         'Content-Type': 'application/json',
         'X-Goog-Api-Key': apiKey,
@@ -17,16 +18,21 @@ def searchNearby():
     'locationRestriction': {
         'circle': {
             'center': {
-                'latitude': 37.7749,
-                'longitude': -122.4194
+                'latitude': lat,
+                'longitude': long
             },
-            'radius': 100.0
+            'radius': radius
         }
     }
 }
     return requests.post(url=urlSearchNearby, headers=headers, json=payload)
     
 
+# Searches using a text query. 
+# the X-Goog-FieldMask header is used to specify which fields are included in the response. 
+# I've included the fields that were relevant at the time of this writing. 
+# However, functionality can be expanded by adding more fields (and updating processData() in csv_helpers.py)
+# A full list of fields can be found here: https://developers.google.com/maps/documentation/places/web-service/text-search#required-parameters
 def searchByText(searchText):
     headers = {
         'Content-Type': 'application/json',
